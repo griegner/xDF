1acd187e4f00286f22e3ad8d2bddc4bb5fc2049d
Gabriel Riegner <gabriel.riegner@gmail.com>
2022-11-16 12:59:42 -0800

    move generate_X to reusable module

--- Python/test_matman.py
+++ Python/test_matman.py
@@ -1,27 +1,6 @@
 import matman
 import numpy as np
-
-
-def _sim_ar1(x, rho=None):
-    """simulate AR(1) process"""
-    x_ar1 = [x[0]]
-    x_ar1.extend(rho * x_ar1[-1] + x_t for x_t in x[1:])
-    return np.array(x_ar1)
-
-
-def _sim_corr(x, y, rho=None):
-    """simulate correlation between timeseries"""
-    return (rho * x) + (y * np.sqrt(1 - rho**2))
-
-
-def _generate_X(n_timepoints=1200, ar_rho=0.5, corr_rho=0.5, rng=None):
-    """generate X matrix (2 regions x n_timepoints) for testing"""
-    x = rng.standard_normal(n_timepoints)  # iid (0, 1)
-    y = rng.standard_normal(n_timepoints)
-    y_corr = _sim_corr(x, y, corr_rho)  # cross correlation
-    x_ar1 = _sim_ar1(x, ar_rho)  # serial correlation
-    y_corr_ar1 = _sim_ar1(y_corr, ar_rho)
-    return np.array([x_ar1, y_corr_ar1])
+from test_data import generate_X
 
 
 def test_corr_mat():
@@ -30,7 +9,7 @@ def test_corr_mat():
     corr_rho = 0.5  # set correlation parameter
     n_timepoints = 1200
     rho_to_z = np.arctanh(corr_rho) * np.sqrt(n_timepoints - 3)
-    X = _generate_X(n_timepoints=n_timepoints, rng=rng)
+    X = generate_X(n_timepoints=n_timepoints, rng=rng)
 
     r_corr, z_corr = matman.corr_mat(X, n_timepoints=n_timepoints)
 
